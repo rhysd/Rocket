d74a3e3000724bfa073b376f9d957c4c8cae9c90
rhysd <lin90162@yahoo.co.jp>
2015-11-13 17:12:05 +0900

    start to implement renderer with React.js

---
+++ renderer/components/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Input from './input';
+
+export default class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className="root">
+                <Input/>
+            </div>
+        );
+    }
+}
---
+++ renderer/components/input.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <input className="search-input" type="search" autoFocus/>
+        );
+    }
+}
+
